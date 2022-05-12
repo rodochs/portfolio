30b0f75b0ea9435cbcf5b866b04dcf69c26cb983
RODOLFO FREITAS ALVES BORGES <rodolfo.fab@gmail.com>
2022-05-11 21:27:15 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -34,18 +34,19 @@ window.addEventListener('DOMContentLoaded', event => {
 });
 
 /*CHART DATA*/
-let day_lab = ['17/April', '18/April', '19/April', '20/April', '21/April', '22/April', '23/April', '24/April', '25/April', '26/April', '27/April', '28/April', '29/April', '30/April', '01/May', '02/May', '03/May', '04/May', '05/May', '06/May', '07/May', '08/May', '09/May', '10/May']
-let day_bolsonaro = [49.65, 39.01, 40.27, 40.31, 39.64, 45.09, 42.72, 42.53, 42.01, 36.79, 46.42, 49.81, 44.81, 44.21, 45.18, 42.95, 44.27, 40.44, 44.67, 40.47, 42.01, 44.71, 40.68, 42.8]
-let day_lula = [41.48, 38.6, 39.17, 35.64, 28.98, 44.2, 36.54, 43.67, 41.52, 43.99, 33.94, 46.61, 40.57, 34.4, 39.54, 43.6, 42.16, 47.14, 43.11, 43.5, 59.79, 49.35, 43.59, 38.86]
-let day_ciro = [49.88, 44.25, 46.39, 45.01, 45.58, 46.08, 42.03, 41.83, 46.56, 47.41, 46.94, 33.59, 35.56, 40.07, 44.23, 48.51, 30.53, 48.83, 44.58, 45.17, 45.29, 61.62, 62.09, 35.87]
-let day_doria = [41.61, 32.2, 39.87, 35.81, 39.82, 42.53, 44.67, 39.27, 38.89, 41.92, 37.35, 34.74, 34.29, 42.64, 31.43, 41.78, 32.42, 41.87, 33.35, 42.05, 52.68, 32.24, 37.63, 43.02]
-let week_lab = ['17/April - 23/April', '24/April - 30/April', '01/May - 07/May']
-let week_bolsonaro = [42.33, 43.73, 43.0]
-let week_lula = [38.01, 41.72, 43.18]
-let week_ciro = [46.2, 41.98, 43.64]
-let week_doria = [38.64, 37.74, 37.15]
+let day_lab = ['17/April', '18/April', '19/April', '20/April', '21/April', '22/April', '23/April', '24/April', '25/April', '26/April', '27/April', '28/April', '29/April', '30/April', '01/May', '02/May', '03/May', '04/May', '05/May', '06/May', '07/May', '08/May', '09/May', '10/May', '11/May']
+let day_bolsonaro = [49.65, 39.01, 40.27, 40.31, 39.64, 45.09, 42.72, 42.53, 42.01, 36.79, 46.42, 49.81, 44.81, 44.21, 45.18, 42.95, 44.27, 40.44, 44.67, 40.47, 42.01, 44.71, 40.68, 42.8, 38.51]
+let day_lula = [41.48, 38.6, 39.17, 35.64, 28.98, 44.2, 36.54, 43.67, 41.52, 43.99, 33.94, 46.61, 40.57, 34.4, 39.54, 43.6, 42.16, 47.14, 43.11, 43.5, 59.79, 49.35, 43.59, 38.86, 37.66]
+let day_ciro = [49.88, 44.25, 46.39, 45.01, 45.58, 46.08, 42.03, 41.83, 46.56, 47.41, 46.94, 33.59, 35.56, 40.07, 44.23, 48.51, 30.53, 48.83, 44.58, 45.17, 45.29, 61.62, 62.09, 35.87, 45.15]
+let day_doria = [41.61, 32.2, 39.87, 35.81, 39.82, 42.53, 44.67, 39.27, 38.89, 41.92, 37.35, 34.74, 34.29, 42.64, 31.43, 41.78, 32.42, 41.87, 33.35, 42.05, 52.68, 32.24, 37.63, 43.02, 44.01]
+let week_lab = ['17/04 - 24/04', '24/04 - 01/05', '01/05 - 08/05']
+let week_bolsonaro = [42.38, 43.8, 42.86]
+let week_lula = [37.8, 40.67, 45.55]
+let week_ciro = [45.6, 41.71, 43.88]
+let week_doria = [39.5, 38.44, 39.37]
 
-let bw = 1
+let bw = 1;
+let pr = 0;
 
 let ctx = document.getElementsByClassName("chart");
 
@@ -60,14 +61,14 @@ let config = {
           borderWidth: bw,
           backgroundColor: "rgba(219, 65, 90, 0.1)",
           data: day_lula,
-          pointRadius: 0,
+          pointRadius: pr,
         }, 
         { 
           label: "Bolsonaro",
           borderColor: "#085ce6",
           borderWidth: bw,
           backgroundColor: "rgba(8, 92, 230, 0.1)",
-          pointRadius: 0,
+          pointRadius: pr,
           data: day_bolsonaro,
         },
         { 
@@ -75,7 +76,7 @@ let config = {
           borderColor: "#2aab1a",
           borderWidth: bw,
           backgroundColor: "rgba(42, 171, 26, 0.1)",
-          pointRadius: 0,
+          pointRadius: pr,
           hidden: "true",
           data: day_ciro,
         },
@@ -84,7 +85,7 @@ let config = {
           borderColor: "#6618ab",
           borderWidth: bw,
           backgroundColor: "rgba(102, 24, 171, 0.1)",
-          pointRadius: 0,
+          pointRadius: pr,
           hidden: "true",
           data: day_doria,
         },
@@ -167,9 +168,16 @@ const day = () => {
     config.data.datasets[2].data = day_ciro
     config.data.datasets[3].data = day_doria
 
-    config.type = 'line'
+    config.data.datasets[0].hidden = false;
+    config.data.datasets[1].hidden = false;
+    config.data.datasets[2].hidden = true;
+    config.data.datasets[3].hidden = true;
+    config.options.tooltips.intersect = false;
 
+    config.type = 'line'
     config.options.scales.xAxes[0].gridLines.display = false;
+    config.options.title.text = 'Brazilian Presidential Candidates - Daily Twitter Score (%)';
+
     window.chart.update();
 };
 
@@ -180,9 +188,21 @@ const week = () => {
     config.data.datasets[2].data = week_ciro;
     config.data.datasets[3].data = week_doria;
 
+    config.data.labels = week_lab;
+    config.data.datasets[0].hidden = false;
+    config.data.datasets[1].hidden = false;
+    config.data.datasets[2].hidden = false;
+    config.data.datasets[3].hidden = false;
+
     config.type = 'bar';
     config.options.scales.xAxes = {};
-    
+    config.options.title.text = 'Brazilian Presidential Candidates - Weekly Twitter Score (%)';
+    config.options.tooltips.intersect = true;
+
+
     window.chart.update();
 
 }
+
+
+
