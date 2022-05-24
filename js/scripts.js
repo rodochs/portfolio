ac0113bfed18eec2d0c507991a56ed4f037544cc
RODOLFO FREITAS ALVES BORGES <rodolfo.fab@gmail.com>
2022-05-23 22:27:15 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -34,16 +34,20 @@ window.addEventListener('DOMContentLoaded', event => {
 });
 
 /*CHART DATA*/
-let day_lab = ['17/April', '18/April', '19/April', '20/April', '21/April', '22/April', '23/April', '24/April', '25/April', '26/April', '27/April', '28/April', '29/April', '30/April', '01/May', '02/May', '03/May', '04/May', '05/May', '06/May', '07/May', '08/May', '09/May', '10/May', '11/May', '12/May', '13/May', '14/May', '16/May', '17/May', '18/May', '19/May', '20/May']
-let day_bolsonaro = [49.65, 39.01, 40.27, 40.31, 39.64, 45.09, 42.72, 42.53, 42.01, 36.79, 46.42, 49.81, 44.81, 44.21, 45.18, 42.95, 44.27, 40.44, 44.67, 40.47, 42.01, 44.71, 40.68, 42.8, 38.51, 39.27, 44.57, 46.57, 41.44, 40.45, 48.09, 44.48, 41.48]
-let day_lula = [41.48, 38.6, 39.17, 35.64, 28.98, 44.2, 36.54, 43.67, 41.52, 43.99, 33.94, 46.61, 40.57, 34.4, 39.54, 43.6, 42.16, 47.14, 43.11, 43.5, 59.79, 49.35, 43.59, 38.86, 37.66, 43.76, 48.51, 39.22, 41.61, 43.74, 45.79, 43.48, 42.05]
-let day_ciro = [49.88, 44.25, 46.39, 45.01, 45.58, 46.08, 42.03, 41.83, 46.56, 47.41, 46.94, 33.59, 35.56, 40.07, 44.23, 48.51, 30.53, 48.83, 44.58, 45.17, 45.29, 61.62, 62.09, 35.87, 45.15, 44.16, 45.94, 41.77, 41.99, 46.82, 38.22, 46.19, 36.64]
-let day_doria = [41.61, 32.2, 39.87, 35.81, 39.82, 42.53, 44.67, 39.27, 38.89, 41.92, 37.35, 34.74, 34.29, 42.64, 31.43, 41.78, 32.42, 41.87, 33.35, 42.05, 52.68, 32.24, 37.63, 43.02, 44.01, 40.71, 37.03, 39.62, 40.16, 36.62, 40.14, 41.95, 36.58]
-let week_lab = ['17/April - 23/April', '24/April - 30/April', '01/May - 07/May', '08/May - 14/May']
-let week_bolsonaro = [42.33, 43.73, 43.0, 41.76]
-let week_lula = [38.01, 41.72, 43.18, 43.62]
-let week_ciro = [46.2, 41.98, 43.64, 49.14]
-let week_doria = [38.64, 37.74, 37.15, 39.11]
+let day_lab = ['17/April', '18/April', '19/April', '20/April', '21/April', '22/April', '23/April', '24/April', '25/April', '26/April', '27/April', '28/April', '29/April', '30/April', '01/May', '02/May', '03/May', '04/May', '05/May', '06/May', '07/May', '08/May', '09/May', '10/May', '11/May', '12/May', '13/May', '14/May', '16/May', '17/May', '18/May', '19/May', '20/May', '21/May', '22/May', '23/May']
+let day_bolsonaro = [49.65, 39.01, 40.27, 40.31, 39.64, 45.09, 42.72, 42.53, 42.01, 36.79, 46.42, 49.81, 44.81, 44.21, 45.18, 42.95, 44.27, 40.44, 44.67, 40.47, 42.01, 44.71, 40.68, 42.8, 38.51, 39.27, 44.57, 46.57, 41.44, 40.45, 48.09, 44.48, 41.48, 42.18, 44.52, 42.81]
+let day_lula = [41.48, 38.6, 39.17, 35.64, 28.98, 44.2, 36.54, 43.67, 41.52, 43.99, 33.94, 46.61, 40.57, 34.4, 39.54, 43.6, 42.16, 47.14, 43.11, 43.5, 59.79, 49.35, 43.59, 38.86, 37.66, 43.76, 48.51, 39.22, 41.61, 43.74, 45.79, 43.48, 42.05, 43.11, 42.23, 41.39]
+let day_ciro = [49.88, 44.25, 46.39, 45.01, 45.58, 46.08, 42.03, 41.83, 46.56, 47.41, 46.94, 33.59, 35.56, 40.07, 44.23, 48.51, 30.53, 48.83, 44.58, 45.17, 45.29, 61.62, 62.09, 35.87, 45.15, 44.16, 45.94, 41.77, 41.99, 46.82, 38.22, 46.19, 36.64, 41.09, 39.3, 44.45]
+/*
+let day_doria = [41.61, 32.2, 39.87, 35.81, 39.82, 42.53, 44.67, 39.27, 38.89, 41.92, 37.35, 34.74, 34.29, 42.64, 31.43, 41.78, 32.42, 41.87, 33.35, 42.05, 52.68, 32.24, 37.63, 43.02, 44.01, 40.71, 37.03, 39.62, 40.16, 36.62, 40.14, 41.95, 36.58, 29.53, 36.29, 38.31]
+*/
+let week_lab = ['17/April - 23/April', '24/April - 30/April', '01/May - 07/May', '08/May - 14/May', '16/May - 22/May']
+let week_bolsonaro = [42.33, 43.73, 43.0, 41.76, 43.02]
+let week_lula = [38.01, 41.72, 43.18, 43.62, 43.3]
+let week_ciro = [46.2, 41.98, 43.64, 49.14, 41.83]
+/*
+let week_doria = [38.64, 37.74, 37.15, 39.11, 37.5]
+*/
 
 let bw = 1;
 let pr = 0;
@@ -80,7 +84,7 @@ let config = {
           hidden: "true",
           data: day_ciro,
         },
-        { 
+        /*{ 
           label: "Doria",
           borderColor: "#6618ab",
           borderWidth: bw,
@@ -88,7 +92,7 @@ let config = {
           pointRadius: pr,
           hidden: "true",
           data: day_doria,
-        },
+        },*/
     ]
   },
   options: {
@@ -166,12 +170,15 @@ const day = () => {
     config.data.datasets[0].data = day_lula
     config.data.datasets[1].data = day_bolsonaro
     config.data.datasets[2].data = day_ciro
+    /*
     config.data.datasets[3].data = day_doria
-
+    */
     config.data.datasets[0].hidden = false;
     config.data.datasets[1].hidden = false;
     config.data.datasets[2].hidden = true;
+    /*
     config.data.datasets[3].hidden = true;
+    */
     config.options.tooltips.intersect = false;
 
     config.type = 'line'
@@ -186,13 +193,17 @@ const week = () => {
     config.data.datasets[0].data = week_lula;
     config.data.datasets[1].data = week_bolsonaro;
     config.data.datasets[2].data = week_ciro;
+    /*
     config.data.datasets[3].data = week_doria;
+    */
 
     config.data.labels = week_lab;
     config.data.datasets[0].hidden = false;
     config.data.datasets[1].hidden = false;
     config.data.datasets[2].hidden = false;
+    /*
     config.data.datasets[3].hidden = false;
+    */
 
     config.type = 'bar';
     config.options.scales.xAxes = {};
